27f70cc2e671163b479049ab8b23428705fa8d7c
Nikolai_Bitkov <Nikolai_Bitkov@epam.com>
2025-03-28 14:56:00 +0100

    add log

--- JenkinsTest/UnitTest1.cs
+++ JenkinsTest/UnitTest1.cs
@@ -1,15 +1,25 @@
+using Xunit.Abstractions;
+
 namespace JenkinsTest
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
         [Fact]
         public void PassingTest()
         {
+            output.WriteLine("This is passing test!");
             Assert.True(true);
         }
         [Fact]
         public void FailedTest()
         {
+            output.WriteLine("This is failing test!");
             Assert.True(false);
         }
     }
